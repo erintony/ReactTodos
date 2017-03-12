a72c8e23c6ce508a0f898d9a199c807bfd837742
ErinTony <VanessaYinmd@163.com>
2017-03-12 21:52:57 +0800

    first react and webpack practise,

---
+++ src/components/TodoItem.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class TodoItem extends React.Component {
+	handlerChange() {
+    let isDone = !this.props.isDone;
+    this.props.changeTodoState(this.props.index, isDone);
+  }
+  handlerDelete() {
+    this.props.deleteTodo(this.props.index)
+  }
+  handlerMouseIn () {
+    ReactDOM.findDOMNode(this.refs.delButton).style.display = 'inline-block'
+  }
+  handlerMouseOut () {
+    ReactDOM.findDOMNode(this.refs.delButton).style.display = 'none'
+  }
+  render() {
+    let className = this.props.isDone?'task-done':''
+    return (
+      <li onMouseOver={this.handlerMouseIn.bind(this)} onMouseOut={this.handlerMouseOut.bind(this)}>
+        <checkbox checked={this.props.isDone} onChange={this.handlerChange.bind(this)}/>
+        <span className="time">{this.props.time}</span>
+        <span className={className+' task'}>{this.props.text}</span>
+        <button ref="delButton" type="danger" size="small" onClick={this.handlerDelete.bind(this)}> 删除 </button>
+      </li>
+    )
+  }
+}
+
+export default TodoItem
